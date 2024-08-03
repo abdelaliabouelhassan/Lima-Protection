8091e14f90a27495be55264592c6b975e6b90903
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2024-08-03 16:25:43 +0100

    sixth section

---
+++ components/partials/home/SixthSection.vue
@@ -0,0 +1,96 @@
+<template>
+  <section class="w-full pt-20 pb-40 space-y-14">
+    <div class="flex flex-col items-center gap-6 px-4 text-center">
+      <span
+        class="font-satoshi font-black text-sm text-white text-opacity-60 uppercase"
+        >our recruitment process</span
+      >
+
+      <h3 class="text-white font-satoshi font-bold text-4xl">
+        Confident? Great, let’s be part of our team
+      </h3>
+
+      <button
+        class="text-black font-satoshi font-medium text-base bg-white py-[12px] px-[24px] rounded-full hover:shadow-[0px_0px_25px_rgba(255,255,255,0.5)] duration-200"
+      >
+        Get started
+      </button>
+    </div>
+
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-[1280px] mx-auto px-4 xl:px-0"
+    >
+      <div class="w-full flex flex-col items-center -space-y-28">
+        <div>
+          <img
+            src="/images/sixthsection/Targeted.svg"
+            class="w-full h-full object-cover"
+            alt=""
+          />
+        </div>
+
+        <div class="w-full text-center space-y-4">
+          <h3 class="text-white font-medium font-satoshi text-xl">
+            Targeted profile
+          </h3>
+          <p
+            class="text-sm text-white text-opacity-70 font-satoshi font-normal"
+          >
+            Recruitment focused on candidates with experience in the police
+            forces, law enforcement agencies (FDO), or armed forces.
+          </p>
+        </div>
+      </div>
+      <div class="w-full flex flex-col items-center -space-y-28">
+        <div>
+          <img
+            src="/images/sixthsection/Specialized.svg"
+            class="w-full h-full object-cover"
+            alt=""
+          />
+        </div>
+
+        <div class="w-full text-center space-y-4">
+          <h3 class="text-white font-medium font-satoshi text-xl">
+            Specialized expertise
+          </h3>
+          <p
+            class="text-sm text-white text-opacity-70 font-satoshi font-normal"
+          >
+            Search for agents with specific skills acquired in the context of
+            their previous functions within police institutions, law enforcement
+            or the army.
+          </p>
+        </div>
+      </div>
+      <div class="w-full flex flex-col items-center -space-y-28">
+        <div>
+          <img
+            src="/images/sixthsection/Rigorous.svg"
+            class="w-full h-full object-cover"
+            alt=""
+          />
+        </div>
+
+        <div class="w-full text-center space-y-4">
+          <h3 class="text-white font-medium font-satoshi text-xl">
+            Rigorous selection
+          </h3>
+          <p
+            class="text-sm text-white text-opacity-70 font-satoshi font-normal"
+          >
+            Careful recruitment process aimed at identifying qualified
+            professionals with the appropriate training to carry out private
+            assistance and close protection missions.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import ClientsCard from "@/components/UI/ClientsCard.vue";
+import ServicesCard from "@/components/UI/ServicesCard.vue";
+import PortfolioCard from "@/components/UI/PortfolioCard.vue";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
     <ThirdSection />
     <FourthSection />
     <FifthSection />
+    <SixthSection />
   </div>
 </template>
 
@@ -14,4 +15,5 @@ import FirstSection from "@/components/partials/home/FirstSection.vue";
 import ThirdSection from "@/components/partials/home/ThirdSection.vue";
 import FourthSection from "@/components/partials/home/FourthSection.vue";
 import FifthSection from "@/components/partials/home/FifthSection.vue";
+import SixthSection from "@/components/partials/home/SixthSection.vue";
 </script>
