241a4d1724524b2bff9a2274216115d1d8ded294
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2024-08-04 13:47:15 +0100

    footer

---
+++ components/partials/Footer.vue
@@ -0,0 +1,157 @@
+<template>
+  <footer class="w-full py-20 space-y-10">
+    <div
+      class="flex items-start md:items-center lg:items-start lg:flex-row flex-col justify-center w-full lg:justify-between max-w-[1280px] px-4 xl:px-0 mx-auto"
+    >
+      <div
+        class="max-w-[372px] w-full flex flex-col items-start md:items-center lg:items-start gap-4"
+      >
+        <div>
+          <img
+            src="/images/logo.svg"
+            class="w-full h-full max-w-[223px] object-cover"
+            alt=""
+          />
+        </div>
+
+        <p class="text-white font-satoshi font-medium">
+          Our security company established in the Paris region, Lima Protection,
+          provides private aid and close security missions both in France and
+          abroad.
+        </p>
+
+        <div class="flex items-center -space-x-3 md:ml-0 lg:-ml-4 -ml-4">
+          <a href="#">
+            <img src="/images/facebook.svg" alt="" />
+          </a>
+          <a href="#">
+            <img src="/images/x.svg" alt="" />
+          </a>
+          <a href="#">
+            <img src="/images/linkding.svg" alt="" />
+          </a>
+          <a href="#">
+            <img src="/images/instagram.svg" alt="" />
+          </a>
+        </div>
+      </div>
+
+      <div
+        class="flex md:flex-row flex-col items-start md:items-start md:justify-between gap-8"
+      >
+        <div class="flex flex-col items-start gap-4">
+          <h4 class="text-lg font-satoshi text-white font-bold">MENU</h4>
+          <a href="#" class="text-white font-satoshi font-medium text-sm">
+            About us
+          </a>
+          <a href="#" class="text-white font-satoshi font-medium text-sm">
+            Services
+          </a>
+          <a href="#" class="text-white font-satoshi font-medium text-sm">
+            Blog
+          </a>
+          <a href="#" class="text-white font-satoshi font-medium text-sm">
+            Customers
+          </a>
+          <a href="#" class="text-white font-satoshi font-medium text-sm">
+            Contact us
+          </a>
+        </div>
+        <div class="flex flex-col items-start gap-4">
+          <h4 class="text-lg font-satoshi text-white font-bold">SERVICES</h4>
+          <a href="#" class="text-white font-satoshi font-medium text-sm">
+            Personalized Risk Assessment
+          </a>
+          <a href="#" class="text-white font-satoshi font-medium text-sm">
+            Comprehensive Security Solutions
+          </a>
+          <a href="#" class="text-white font-satoshi font-medium text-sm">
+            Proactive Monitoring
+          </a>
+        </div>
+        <div class="flex flex-col items-start gap-4">
+          <h4 class="text-lg font-satoshi text-white font-bold">CONTACT US</h4>
+          <div class="-space-y-4 -mt-4">
+            <div class="flex items-center -space-x-3 -space-y-4 gap-2 -ml-5">
+              <div>
+                <img src="/images/tele.svg" alt="" />
+              </div>
+              <a
+                href="tel:012345678"
+                class="text-white font-satoshi font-medium text-sm"
+              >
+                012345678
+              </a>
+            </div>
+            <div class="flex items-center -space-x-3 -space-y-4 gap-2 -ml-5">
+              <div>
+                <img src="/images/mail.svg" alt="" />
+              </div>
+              <a
+                href="mailto:abcxyz@gmail.com"
+                class="text-white font-satoshi font-medium text-sm"
+              >
+                abcxyz@gmail.com
+              </a>
+            </div>
+            <div class="flex items-center -space-x-3 -space-y-4 gap-2 -ml-5">
+              <div>
+                <img src="/images/gpss.svg" alt="" />
+              </div>
+              <a href="#" class="text-white font-satoshi font-medium text-sm">
+                Random address here
+              </a>
+            </div>
+          </div>
+          <button
+            class="text-black -mt-2 hover:shadow-[0px_0px_25px_rgba(255,255,255,0.5)] font-satoshi font-medium bg-white py-[12px] px-[24px] rounded-full duration-200"
+          >
+            Contact us
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="max-w-[1280px] mx-auto py-8 border-y border-[#FFFFFF4D] px-4 xl:px-0"
+    >
+      <p class="text-white font-medium font-satoshi text-sm text-center">
+        CNAPS AUT-077-2123-04-26-20240925398 <br />
+        Article L. 612-14 du Code de la sécurité intérieure : « L'autorisation
+        d'exploitation ne confère aucune prérogative de puissance publique à
+        l'entreprise ou aux personnes qui en bénéficient.
+      </p>
+    </div>
+
+    <div
+      class="max-w-[1280px] mx-auto flex items-center md:flex-row flex-col gap-4 md:justify-between px-4 xl:px-0"
+    >
+      <p class="text-white font-medium font-satoshi text-sm">
+        © 2024 Lima Protection. All rights reserved.
+      </p>
+
+      <div class="flex items-center gap-4">
+        <a
+          href="#"
+          class="text-white font-medium font-satoshi text-sm underline"
+          >Cookies</a
+        >
+        <a
+          href="#"
+          class="text-white font-medium font-satoshi text-sm underline"
+          >Privacy</a
+        >
+        <a
+          href="#"
+          class="text-white font-medium font-satoshi text-sm underline"
+          >Terms & Services</a
+        >
+        <a
+          href="#"
+          class="text-white font-medium font-satoshi text-sm underline"
+          >Sitemap</a
+        >
+      </div>
+    </div>
+  </footer>
+</template>
--- components/partials/home/NinthSection.vue
+++ components/partials/home/NinthSection.vue
@@ -1,35 +1,35 @@
 <template>
-  <section
-    class="w-full relative overflow-hidden h-[55rem] sm:h-[45rem] md:h-[40rem] -space--20"
-  >
-    <div class="absolute w-full top-28">
-      <div class="flex flex-col items-center gap-6 px-4 text-center pt-4">
-        <span
-          class="font-satoshi font-black text-sm text-white text-opacity-60 uppercase"
-          >Let's get started</span
-        >
-
-        <h3 class="text-white font-satoshi font-bold text-4xl">
-          We're ready to fulfill your security needs
-        </h3>
-
-        <button class="element rounded-full p-px group duration-300">
-          <div
-            class="rounded-full py-[12px] px-[20px] text-white font-satoshi font-medium bg-black duration-300 bg-gradient-to-br group-hover:from-[#ffffff80] group-hover:to-[#00000001] from-[#ffffff30] from-[9.11%] to-[#00000001] to-[76.61%]"
+  <section class="w-full -space-y-60">
+    <div class="w-full relative overflow-hidden h-[55rem]">
+      <div class="absolute w-full top-28">
+        <div class="flex flex-col items-center gap-6 px-4 text-center pt-4">
+          <span
+            class="font-satoshi font-black text-sm text-white text-opacity-60 uppercase"
+            >Let's get started</span
           >
-            Hire us
-          </div>
-        </button>
+
+          <h3 class="text-white font-satoshi font-bold text-4xl">
+            We're ready to fulfill your security needs
+          </h3>
+
+          <button class="element rounded-full p-px group duration-300">
+            <div
+              class="rounded-full py-[12px] px-[20px] text-white font-satoshi font-medium bg-black duration-300 bg-gradient-to-br group-hover:from-[#ffffff80] group-hover:to-[#00000001] from-[#ffffff30] from-[9.11%] to-[#00000001] to-[76.61%]"
+            >
+              Hire us
+            </div>
+          </button>
+        </div>
       </div>
+
+      <img
+        src="/images/rounded-effect.png"
+        class="w-full h-full object-cover mt-40"
+        alt=""
+      />
     </div>
 
-    <img
-      src="/images/rounded-effect.png"
-      class="w-full h-full object-cover mt-40"
-      alt=""
-    />
-
-    <div class="absolute w-full bottom-0">
+    <div class="relative z-20">
       <div
         class="w-full max-w-[1180px] mx-auto text-center px-4 pb-4 space-y-8"
       >
